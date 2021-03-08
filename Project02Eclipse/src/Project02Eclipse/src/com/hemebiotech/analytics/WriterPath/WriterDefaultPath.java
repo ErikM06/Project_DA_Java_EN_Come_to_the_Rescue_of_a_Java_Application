ff155a90da9739a146b1f103e738dcdf54ee8725
Erik Murer <murer.erik@gmail.com>
2021-03-08 10:26:12 +0100

    Creating the output default path (given in main)

--- Project02Eclipse/src/Project02Eclipse/src/com/hemebiotech/analytics/WriterPath/WriterDefaultPath.java
+++ Project02Eclipse/src/Project02Eclipse/src/com/hemebiotech/analytics/WriterPath/WriterDefaultPath.java
@@ -2,6 +2,8 @@ package Project02Eclipse.src.com.hemebiotech.analytics.WriterPath;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 
 public class WriterDefaultPath implements IWriterDefaultPath {
@@ -11,19 +13,27 @@ public class WriterDefaultPath implements IWriterDefaultPath {
 	 */
 	public void GetWriterDefaultPath(String outputDefaultFilePath) {
 		
+		Writer fw = null;
 		try {
-		File file = new File (outputDefaultFilePath);
-		file.getParentFile().mkdirs(); // create all the directories if they don't already exist
-		FileWriter fw = new FileWriter (file);
-		System.out.println("path is "+ file.getAbsolutePath());
-		fw.close();
+			File file = new File (outputDefaultFilePath);
+			file.getParentFile().mkdirs(); // create all the directories if they don't already exist
+			fw = new FileWriter (file);
+			System.out.println("path is "+ file.getAbsolutePath());
+			
 		}
 		catch (Exception e){
-		System.out.println("Default path couldn't be created");
+			System.out.println("Default path couldn't be created");
 		}
 		
+		
 		finally {
-		System.out.println("The try finished");
+			System.out.println("The try finished");
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
